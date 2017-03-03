2a428f320e5760f64f5179029abb751bea85aa36
tylerlaberge <tyler.laberge@maine.edu>
2017-03-03 01:11:29 -0500

    fixed typo in setup

--- setup.py
+++ setup.py
@@ -11,9 +11,10 @@ setup(
     install_requires=['click==6.7', 'termcolor==1.1.0', 'tqdm==4.11.2', 'colorama==0.3.7'],
     author='Tyler LaBerge',
     author_email='tyler.laberge@maine.edu',
-    description='An asynchronous behaviour-driven development framework',
-    keywords='python async asynchronous test-driven behaviour-driven development framework '
-             'unittest testing assertion library bdd tdd tests test behaviour',
+    description='An asynchronous behavior-driven development framework',
+    keywords='python async asynchronous test test-driven-development behavior behavior-driven-development tdd bdd '
+             'testing test-automation test-framework testing-tools tests framework library',
     url='https://github.com/tylerlaberge/Jasper',
     license='MIT'
 )
+

b2491ac823f4632622fce1336d0be610dc2220e9
tylerlaberge <tyler.laberge@maine.edu>
2017-03-03 13:50:41 -0500

    modified expect api, added <= and >= expectation functions. updated tests.

--- jasper/expect.py
+++ jasper/expect.py
@@ -17,94 +17,84 @@ class Expect(object):
         """
         self.actual_data = actual_data
         self.negate = False
-        self.operator = None
 
-    def __call__(self, expected_data):
-        """
-        Make an assertion on based on the current operator.
-
-        :param expected_data: The expected data to make assertions against the actual data with.
-        :return: This Expect object.
-        :raise ExpectationException: If the expectation fails.
-        """
-        if self.operator == 'to_be':
-            if self.negate and self.actual_data is expected_data:
-                raise ExpectationException(self.actual_data, expected_data, 'not_to_be')
-            elif not self.negate and self.actual_data is not expected_data:
-                raise ExpectationException(self.actual_data, expected_data, 'to_be')
-
-        elif self.operator == 'to_equal':
-            if self.negate and self.actual_data == expected_data:
-                raise ExpectationException(self.actual_data, expected_data, 'not_to_equal')
-            elif not self.negate and self.actual_data != expected_data:
-                raise ExpectationException(self.actual_data, expected_data, 'to_equal')
-
-        return self
-
-    @property
     def not_(self):
         """
-        Negate any operator that is set.
+        Negate any future expectations.
 
         :return: This Expect object.
         """
         self.negate = True
         return self
 
-    @property
-    def to_be(self):
+    def to_be(self, expected_data):
         """
-        Set the operator of this Expect object to the identity operator.
+        Make an assertion that this objects actual data is the expected data.
 
-        Can also be used to chain into the less_than or greater_than assertions.
-
-        :return: This Expect object.
+        :param expected_data: The data to expect the actual data to be.
+        :raise ExpectationException: If the expectation fails.
         """
-        self.operator = 'to_be'
-        return self
+        if self.negate and self.actual_data is expected_data:
+            raise ExpectationException(self.actual_data, expected_data, 'not_to_be')
+        elif not self.negate and self.actual_data is not expected_data:
+            raise ExpectationException(self.actual_data, expected_data, 'to_be')
 
-    @property
-    def to_equal(self):
+    def to_equal(self, expected_data):
         """
-        Set the operator of this Expect object to the equality operator.
+        Make an assertion that this objects actual data is equal to the expected data.
 
-        :return: This Expect object.
+        :param expected_data: The data to expect the actual data to be equal to.
+        :raise ExpectationException: If the expectation fails.
         """
-        self.operator = 'to_equal'
-        return self
+        if self.negate and self.actual_data == expected_data:
+            raise ExpectationException(self.actual_data, expected_data, 'not_to_equal')
+        elif not self.negate and self.actual_data != expected_data:
+            raise ExpectationException(self.actual_data, expected_data, 'to_equal')
 
-    def less_than(self, expected_data):
+    def to_be_less_than(self, expected_data):
         """
         Make an assertion that this objects actual data is less than the expected data.
 
-        The to_be operator must be set in order to use this assertion.
-
         :param expected_data: The data to expect the actual data to be less than.
-        :return: This Expect object.
         :raise ExpectationException: If the expectation fails.
         """
-        if self.operator == 'to_be':
-            if self.negate and self.actual_data < expected_data:
-                raise ExpectationException(self.actual_data, expected_data, 'not_to_be_less_than')
-            elif not self.negate and not self.actual_data < expected_data:
-                raise ExpectationException(self.actual_data, expected_data, 'to_be_less_than')
+        if self.negate and self.actual_data < expected_data:
+            raise ExpectationException(self.actual_data, expected_data, 'not_to_be_less_than')
+        elif not self.negate and not self.actual_data < expected_data:
+            raise ExpectationException(self.actual_data, expected_data, 'to_be_less_than')
 
-        return self
-
-    def greater_than(self, expected_data):
+    def to_be_greater_than(self, expected_data):
         """
         Make an assertion that this objects actual data is greater than the expected data.
 
-        The to_be operator must be set in order to use this assertion.
-
         :param expected_data: The data to expect the actual data to be less than.
-        :return: This Expect object.
         :raise ExpectationException: If the expectation fails.
         """
-        if self.operator == 'to_be':
-            if self.negate and self.actual_data > expected_data:
-                raise ExpectationException(self.actual_data, expected_data, 'not_to_be_greater_than')
-            elif not self.negate and not self.actual_data > expected_data:
-                raise ExpectationException(self.actual_data, expected_data, 'to_be_greater_than')
+        if self.negate and self.actual_data > expected_data:
+            raise ExpectationException(self.actual_data, expected_data, 'not_to_be_greater_than')
+        elif not self.negate and not self.actual_data > expected_data:
+            raise ExpectationException(self.actual_data, expected_data, 'to_be_greater_than')
 
-        return self
+    def to_be_less_than_or_equal_to(self, expected_data):
+        """
+        Make an assertion that this objects actual data is less than or equal to the expected data
+
+        :param expected_data: The data to expect the actual data to be less than or equal to.
+        :raise ExpectationException: If the expectation fails.
+        """
+        if self.negate and self.actual_data <= expected_data:
+            raise ExpectationException(self.actual_data, expected_data, 'not_to_be_less_than_or_equal_to')
+        elif not self.negate and not self.actual_data <= expected_data:
+            raise ExpectationException(self.actual_data, expected_data, 'to_be_less_than_or_equal_to')
+
+    def to_be_greater_than_or_equal_to(self, expected_data):
+        """
+        Make an assertion that this objects actual data is greater than or equal to the expected data
+
+        :param expected_data: The data to expect the actual data to be greater than or equal to.
+        :raise ExpectationException: If the expectation fails.
+        """
+        if self.negate and self.actual_data >= expected_data:
+            raise ExpectationException(self.actual_data, expected_data, 'not_to_be_greater_than_or_equal_to')
+        elif not self.negate and not self.actual_data >= expected_data:
+            raise ExpectationException(self.actual_data, expected_data, 'to_be_greater_than_or_equal_to')
--- tests/test_unit/test_expect.py
+++ tests/test_unit/test_expect.py
@@ -13,171 +13,208 @@ class ExpectTestCase(TestCase):
 
         self.assertEqual(expect_object.actual_data, self.data)
         self.assertFalse(expect_object.negate)
-        self.assertIsNone(expect_object.operator)
-
-    def test_to_be(self):
-        expect_object = Expect(self.data).to_be
-
-        self.assertTrue(expect_object.operator, 'to_be')
-        self.assertFalse(expect_object.negate)
-
-    def test_to_equal(self):
-        expect_object = Expect(self.data).to_equal
-
-        self.assertEqual(expect_object.operator, 'to_equal')
-        self.assertFalse(expect_object.negate)
 
     def test_not(self):
-        expect_object = Expect(self.data).not_
+        expect_object = Expect(self.data).not_()
 
         self.assertTrue(expect_object.negate)
 
-    def test_not_to_be(self):
-        expect_object = Expect(self.data).not_.to_be
-
-        self.assertTrue(expect_object.negate)
-        self.assertEqual(expect_object.operator, 'to_be')
-
-    def test_successful_to_be_call(self):
+    def test_successful_to_be(self):
         expect_object = Expect(self.data)
-        expect_object.operator = 'to_be'
         expect_object.negate = False
 
         try:
-            expect_object(self.data)
+            expect_object.to_be(self.data)
         except ExpectationException:
             raise AssertionError
 
     def test_failure_to_be_call(self):
         expect_object = Expect(self.data)
-        expect_object.operator = 'to_be'
         expect_object.negate = False
 
         with self.assertRaises(ExpectationException):
-            expect_object({'foo': 'bar'})
+            expect_object.to_be({'foo': 'bar'})
 
     def test_successful_not_to_be_call(self):
         expect_object = Expect(self.data)
-        expect_object.operator = 'to_be'
         expect_object.negate = True
 
         try:
-            expect_object({'foo': 'bar'})
+            expect_object.to_be({'foo': 'bar'})
         except ExpectationException:
             raise AssertionError
 
     def test_failure_not_to_be_call(self):
         expect_object = Expect(self.data)
-        expect_object.operator = 'to_be'
         expect_object.negate = True
 
         with self.assertRaises(ExpectationException):
-            expect_object(self.data)
+            expect_object.to_be(self.data)
 
     def test_successful_to_equal_call(self):
         expect_object = Expect(self.data)
-        expect_object.operator = 'to_equal'
         expect_object.negate = False
 
         try:
-            expect_object({'foo': 'bar'})
+            expect_object.to_equal({'foo': 'bar'})
         except ExpectationException:
             raise AssertionError
 
     def test_failure_to_equal_call(self):
         expect_object = Expect(self.data)
-        expect_object.operator = 'to_equal'
         expect_object.negate = False
 
         with self.assertRaises(ExpectationException):
-            expect_object('foo')
+            expect_object.to_equal('foo')
 
     def test_successful_not_to_equal(self):
         expect_object = Expect(self.data)
-        expect_object.operator = 'to_equal'
         expect_object.negate = True
 
         try:
-            expect_object('foo')
+            expect_object.to_equal('foo')
         except ExpectationException:
             raise AssertionError
 
     def test_failure_not_to_equal(self):
         expect_object = Expect(self.data)
-        expect_object.operator = 'to_equal'
         expect_object.negate = True
 
         with self.assertRaises(ExpectationException):
-            expect_object({'foo': 'bar'})
+            expect_object.to_equal({'foo': 'bar'})
 
     def test_successful_to_be_less_than(self):
         expect_object = Expect(10)
-        expect_object.operator = 'to_be'
         expect_object.negate = False
 
         try:
-            expect_object.less_than(15)
+            expect_object.to_be_less_than(15)
         except ExpectationException:
             raise AssertionError
 
     def test_failure_to_be_less_than(self):
         expect_object = Expect(10)
-        expect_object.operator = 'to_be'
         expect_object.negate = False
 
         with self.assertRaises(ExpectationException):
-            expect_object.less_than(5)
+            expect_object.to_be_less_than(5)
 
     def test_successful_not_to_be_less_than(self):
         expect_object = Expect(10)
-        expect_object.operator = 'to_be'
         expect_object.negate = True
 
         try:
-            expect_object(5)
+            expect_object.to_be_less_than(5)
         except ExpectationException:
             raise AssertionError
 
     def test_failure_not_to_be_less_than(self):
         expect_object = Expect(10)
-        expect_object.operator = 'to_be'
         expect_object.negate = True
 
         with self.assertRaises(ExpectationException):
-            expect_object.less_than(15)
+            expect_object.to_be_less_than(15)
 
     def test_successful_to_be_greater_than(self):
         expect_object = Expect(10)
-        expect_object.operator = 'to_be'
         expect_object.negate = False
 
         try:
-            expect_object.greater_than(5)
+            expect_object.to_be_greater_than(5)
         except ExpectationException:
             raise AssertionError
 
     def test_failure_to_be_greater_than(self):
         expect_object = Expect(10)
-        expect_object.operator = 'to_be'
         expect_object.negate = False
 
         with self.assertRaises(ExpectationException):
-            expect_object.greater_than(15)
+            expect_object.to_be_greater_than(15)
 
     def test_successful_not_to_be_greater_than(self):
         expect_object = Expect(10)
-        expect_object.operator = 'to_be'
         expect_object.negate = True
 
         try:
-            expect_object.greater_than(15)
+            expect_object.to_be_greater_than(15)
         except ExpectationException:
             raise AssertionError
 
     def test_failure_not_to_be_greater_than(self):
         expect_object = Expect(10)
-        expect_object.operator = 'to_be'
         expect_object.negate = True
 
         with self.assertRaises(ExpectationException):
-            expect_object.greater_than(5)
+            expect_object.to_be_greater_than(5)
+
+    def test_successful_to_be_less_than_or_equal_to(self):
+        expect_object = Expect(10)
+        expect_object.negate = False
+
+        try:
+            expect_object.to_be_less_than_or_equal_to(15)
+            expect_object.to_be_less_than_or_equal_to(10)
+        except ExpectationException:
+            raise AssertionError
+
+    def test_failure_to_be_less_than_or_equal_to(self):
+        expect_object = Expect(10)
+        expect_object.negate = False
+
+        with self.assertRaises(ExpectationException):
+            expect_object.to_be_less_than_or_equal_to(9)
+
+    def test_successful_not_to_be_less_than_or_equal_to(self):
+        expect_object = Expect(10)
+        expect_object.negate = True
+
+        try:
+            expect_object.to_be_less_than_or_equal_to(9)
+        except ExpectationException:
+            raise AssertionError
+
+    def test_failure_not_to_be_less_than_or_equal_to(self):
+        expect_object = Expect(10)
+        expect_object.negate = True
+
+        with self.assertRaises(ExpectationException):
+            expect_object.to_be_less_than_or_equal_to(10)
+
+        with self.assertRaises(ExpectationException):
+            expect_object.to_be_less_than_or_equal_to(15)
+
+    def test_successful_to_be_greater_than_or_equal_to(self):
+        expect_object = Expect(10)
+        expect_object.negate = False
+
+        try:
+            expect_object.to_be_greater_than_or_equal_to(5)
+            expect_object.to_be_greater_than_or_equal_to(10)
+        except ExpectationException:
+            raise AssertionError
+
+    def test_failure_to_be_greater_than_or_equal_to(self):
+        expect_object = Expect(10)
+        expect_object.negate = False
+
+        with self.assertRaises(ExpectationException):
+            expect_object.to_be_greater_than_or_equal_to(11)
+
+    def test_successful_not_to_be_greater_than_or_equal_to(self):
+        expect_object = Expect(10)
+        expect_object.negate = True
+
+        try:
+            expect_object.to_be_greater_than_or_equal_to(11)
+        except ExpectationException:
+            raise AssertionError
+
+    def test_failure_not_to_be_greater_than_or_equal_to(self):
+        expect_object = Expect(10)
+        expect_object.negate = True
+
+        with self.assertRaises(ExpectationException):
+            expect_object.to_be_greater_than_or_equal_to(5)
+
+        with self.assertRaises(ExpectationException):
+            expect_object.to_be_greater_than_or_equal_to(10)
